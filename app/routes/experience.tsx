fd66987459de3c0f95f143c84500fe1ce1a24dd3
Vishal Kotcherlakota <dishbreak@gmail.com>
2026-06-10 22:51:28 -0700

    more tweaks to experience

--- app/routes/experience.tsx
+++ app/routes/experience.tsx
@@ -29,13 +29,13 @@ export default function Experience({ loaderData }: Route.ComponentProps): React.
             <div className="sm:flex-1/3">
                 <Heading>Skills</Heading>
                 <div className="max-sm:flex">
-                    <div>
+                    <div className="max-sm:p-4 max-sm:flex-1 max-sm:bg-stone-900 max-sm:shadow-orange-300/15 max-sm:shadow-md">
                         <div className="text-xl font-bold text-orange-300 pt-2 pb-2">Languages</div>
                         <ul className="list-disc list-outside ml-6">
                             {loaderData.skills.languages.map((l, i) => <li key={`lang-${i}`}>{l}</li>)}
                         </ul>
                     </div>
-                    <div className="max-sm:ml-20">
+                    <div className="max-sm:ml-10 max-sm:p-4 max-sm:flex-1 max-sm:bg-stone-900 max-sm:shadow-orange-300/15 max-sm:shadow-md">
                         <div className="text-xl font-bold text-orange-300 pt-2 pb-2">Tools</div>
                         <ul className="list-disc list-outside ml-6">
                             {loaderData.skills.tools.map((t, i) => <li key={`tool-${i}`}>{t}</li>)}
