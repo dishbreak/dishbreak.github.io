7f5e19cb1c638abef29e5ee96b0c1ce83025e1aa
Vishal Kotcherlakota <dishbreak@gmail.com>
2026-06-10 22:54:41 -0700

    fix homescreen responsiveness

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             <div className="py-4 px-15 w-full bg-stone-700 ">
                 <div style={{ opacity: 1 }} className="text-stone-300 text-5xl">Vishal Kotcherlakota</div>
             </div>
-            <div className="px-10 w-full bg-stone-500 text-orange-200 text-2xl flex sm:flex-col justify-end">
+            <div className="px-10 w-full bg-stone-500 text-orange-200 text-2xl flex max-sm:flex-col justify-end">
                 {links.map((n, i) => <Link to={`/${n}`} key={`link-${i}`} className="p-2 hover:bg-orange-400 hover:text-stone-700 hover:cursor-pointer" style={{ opacity: 1 }}>{n.toUpperCase()}</Link>)}
             </div>
         </div>
