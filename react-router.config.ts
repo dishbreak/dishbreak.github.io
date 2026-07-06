1277a046d2830c4c6a172678e09db02f0d5b400d
Vishal Kotcherlakota <vkotcherlakota@nerdwallet.com>
2026-07-06 14:29:10 -0700

    final fix

--- react-router.config.ts
+++ react-router.config.ts
@@ -22,11 +22,4 @@ export default {
       ...slugs,
     ]
   },
-  future: {
-    v8_middleware: true,
-    v8_passThroughRequests: true,
-    v8_splitRouteModules: true,
-    v8_trailingSlashAwareDataRequests: true,
-    v8_viteEnvironmentApi: true,
-  },
 } satisfies Config;
--- vite.config.ts
+++ vite.config.ts
@@ -6,29 +6,25 @@ import { loadAllPosts } from "./lib/posts";
 import { JSDOM } from "jsdom"
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    reactRouter(),
-    rssPlugin({
-      mode: "define",
-      channel: {
-        title: "Vishal Kotcherlakota",
-        link: "https://kotcherlakota.org/"
-      },
-      items: (await loadAllPosts()).map(p => {
-        const dom = new JSDOM()
-        const parser = new dom.window.DOMParser()
-        const doc = parser.parseFromString(p.cleanHTML, "text/html")
-        const description = doc.querySelector("p")?.outerHTML
-        return {
-          title: p.attrs["title"] ?? "(untitled)",
-          link: `https://kotcherlakota.org/blog/${p.slug}`,
-          pubDate: p.posted,
-          description
-        }
-      })
+  plugins: [tailwindcss(), reactRouter(), rssPlugin({
+    mode: "define",
+    channel: {
+      title: "Vishal Kotcherlakota",
+      link: "https://kotcherlakota.org/"
+    },
+    items: (await loadAllPosts()).map(p => {
+      const dom = new JSDOM()
+      const parser = new dom.window.DOMParser()
+      const doc = parser.parseFromString(p.cleanHTML, "text/html")
+      const description = doc.querySelector("p")?.outerHTML
+      return {
+        title: p.attrs["title"] ?? "(untitled)",
+        link: `https://kotcherlakota.org/blog/${p.slug}`,
+        pubDate: p.posted,
+        description
+      }
     })
-  ],
+  })],
   resolve: {
     tsconfigPaths: true,
   },
