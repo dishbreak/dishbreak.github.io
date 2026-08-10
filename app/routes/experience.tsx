ea41e9b0a2cec295362f8ab1b1535279322d96ca
Vishal Kotcherlakota <vkotcherlakota@nerdwallet.com>
2026-08-10 13:54:25 -0700

    update resume

--- app/routes/experience.tsx
+++ app/routes/experience.tsx
@@ -17,7 +17,7 @@ export default function Experience({ loaderData }: Route.ComponentProps): React.
         <P>{loaderData.objective}</P>
 
         <div className="flex max-sm:flex-col">
-            <div className="sm:flex-2/3 sm:mr-2">
+            <div className="sm:flex-2/3 sm:mr-5">
                 <Heading>Professional Summary</Heading>
                 <BulletList>
                     {loaderData.summary.map((d, i) => <li key={`summary-${i}`}>{d}</li>)}
@@ -28,16 +28,16 @@ export default function Experience({ loaderData }: Route.ComponentProps): React.
                 </BulletList>
             </div>
             <div className="sm:flex-1/3">
-                <Heading>Skills</Heading>
+                <Heading padding="pt-8 pb-2">Skills</Heading>
                 <div className="max-sm:flex">
                     <Card>
-                        <Heading size="text-xl">Languages</Heading>
+                        <Heading size="text-xl" padding="pt-2 pb-1">Languages</Heading>
                         <BulletList>
                             {loaderData.skills.languages.map((l, i) => <li key={`lang-${i}`}>{l}</li>)}
                         </BulletList>
                     </Card>
                     <Card>
-                        <Heading size="text-xl">Tools</Heading>
+                        <Heading size="text-xl" padding="pt-2 pt-1">Tools</Heading>
                         <BulletList>
                             {loaderData.skills.tools.map((t, i) => <li key={`tool-${i}`}>{t}</li>)}
                         </BulletList>
@@ -61,10 +61,16 @@ export default function Experience({ loaderData }: Route.ComponentProps): React.
                             {p.key_points.map((d, k) =>
                                 <li key={`summary-${i}-${j}-${k}`}>{d}</li>)}
                         </BulletList>
+                        <p className="pt-2">
+                            <span className="font-bold text-orange-300">Key Technologies: </span>
+                            {p.key_technologies.join(", ")}
+                        </p>
                     </div>)
                 }
             </div>)
         }
+        <Heading>Keywords</Heading>
+        {loaderData.keywords.join(", ")}
     </div>
 }
 
@@ -79,8 +85,8 @@ function toMonthYear(d: string | undefined): string {
     return new Intl.DateTimeFormat("en-US", { month: "long", year: "numeric" }).format(new Date(d))
 }
 
-function Heading({ children, size = "text-3xl" }: { children: React.ReactNode, size?: string }): React.JSX.Element {
-    return <div className={`font-bold text-orange-300 ${size} pt-8 pb-4`}>{children}</div>
+function Heading({ children, size = "text-3xl", padding = "pt-8 pb-4" }: { children: React.ReactNode, size?: string, padding?: string }): React.JSX.Element {
+    return <div className={`font-bold text-orange-300 ${size} ${padding}`}>{children}</div>
 }
 
 function P({ children }: { children: React.ReactNode }): React.JSX.Element {
